6fe76da1374c4fe0d706163727b98486b259e8a3
kinako1415 <Owata09@outlook.jp>
2025-03-15 23:04:44 +0900

    Sidebarコンテンツの枠組みの作成

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import styles from "./Sidebar.module.css";
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.header}>
+        <h1>ノート</h1>
+        <button>追加</button>
+        <div className={styles.notes}>
+          <div className={styles.note}>
+            <div className={styles.title}>
+              <strong>タイトル</strong>
+              <button>削除</button>
+            </div>
+            <p>ノートの内容です</p>
+            <small>最後の修正日: XX/XX</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
