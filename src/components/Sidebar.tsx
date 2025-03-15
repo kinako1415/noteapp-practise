093589ca0a1b21376eb06c2c29b22b59876fa371
kinako1415 <Owata09@outlook.jp>
2025-03-16 00:23:25 +0900

    Sidebarのデザインを作成

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,15 +6,15 @@ const Sidebar = () => {
       <div className={styles.header}>
         <h1>ノート</h1>
         <button>追加</button>
-        <div className={styles.notes}>
-          <div className={styles.note}>
-            <div className={styles.title}>
-              <strong>タイトル</strong>
-              <button>削除</button>
-            </div>
-            <p>ノートの内容です</p>
-            <small>最後の修正日: XX/XX</small>
+      </div>
+      <div className={styles.notes}>
+        <div className={styles.note}>
+          <div className={styles.title}>
+            <strong>タイトル</strong>
+            <button>削除</button>
           </div>
+          <p>ノートの内容です</p>
+          <small>最後の修正日: XX/XX</small>
         </div>
       </div>
     </div>
