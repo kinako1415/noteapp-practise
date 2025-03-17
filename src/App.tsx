d46a9af05656e4297756c0d87acae533e746df3d
kinako1415 <Owata09@outlook.jp>
2025-03-17 15:37:56 +0900

    notesの内容をsidebarに出力

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      <Sidebar onAddNote={onAddNote} />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,19 @@
 import styles from "./Sidebar.module.css";
 
-const Sidebar = ({ onAddNote }: { onAddNote: () => void }) => {
+type newNoteType = {
+  id: number;
+  title: string;
+  content: string;
+  modDate: number;
+};
+
+const Sidebar = ({
+  onAddNote,
+  notes,
+}: {
+  onAddNote: () => void;
+  notes: newNoteType[];
+}) => {
   return (
     <div className={styles.sidebar}>
       <div className={styles.header}>
@@ -8,14 +21,24 @@ const Sidebar = ({ onAddNote }: { onAddNote: () => void }) => {
         <button onClick={onAddNote}>追加</button>
       </div>
       <div className={styles.notes}>
-        <div className={styles.note}>
-          <div className={styles.title}>
-            <strong>タイトル</strong>
-            <button>削除</button>
-          </div>
-          <p>ノートの内容です</p>
-          <small>最後の修正日: XX/XX</small>
-        </div>
+        {notes.map((note, index) => {
+          return (
+            <div key={index} className={styles.note}>
+              <div className={styles.title}>
+                <strong>{note.title}</strong>
+                <button>削除</button>
+              </div>
+              <p>{note.content}</p>
+              <small>
+                最後の修正日:{" "}
+                {new Date(note.modDate).toLocaleDateString("ja-JP", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </small>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
