ff4f41ce24522a0f007a0a9de8250a8ae98bf22c
kinako1415 <Owata09@outlook.jp>
2025-03-19 14:03:57 +0900

    mainコンポーネントの枠作成

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,18 @@
-// import styles from "./Main.module.css";
+import styles from "./Main.module.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.mainNoteEdit}>
+        <input type="text" />
+        <textarea id="" placeholder="ノート内容を記入"></textarea>
+      </div>
+      <div className={styles.mainNotePrev}>
+        <h1 className={styles.prevTitle}>タイトル</h1>
+        <div className={styles.markdownPrev}>ノート内容</div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
